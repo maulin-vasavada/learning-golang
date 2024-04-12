ef121cf05cf21ce179462d11d28b8544c6c2f669
mvasavada <mvasavada@paypal.com>
2024-04-12 16:31:10 -0700

    testing

--- headfirst-go/prose/join_test.go
+++ headfirst-go/prose/join_test.go
@@ -5,6 +5,26 @@ import (
 	"testing"
 )
 
+type testData struct {
+	list []string
+	want string
+}
+
+func TestJoinWithSeparator(t *testing.T) {
+	tests := []testData{
+		testData{list: []string{}, want: ""},
+		testData{list: []string{"apple"}, want: "apple"},
+		testData{list: []string{"apple", "orange"}, want: "apple and orange"},
+		testData{list: []string{"apple", "orange", "pear"}, want: "apple, orange, and pear"},
+	}
+	for _, test := range tests {
+		got := JoinWithSeparator(test.list, ",")
+		if got != test.want {
+			t.Errorf(errorString(test.list, got, test.want))
+		}
+	}
+}
+
 func TestOneElement(t *testing.T) {
 	list := []string{"apple"}
 	want := "apple"
--- headfirst-go/prose/prose.go
+++ headfirst-go/prose/prose.go
@@ -3,7 +3,9 @@ package prose
 import "strings"
 
 func JoinWithSeparator(phrases []string, separator string) string {
-	if len(phrases) == 1 {
+	if len(phrases) == 0 {
+		return ""
+	} else if len(phrases) == 1 {
 		return phrases[0]
 	} else if len(phrases) == 2 {
 		return phrases[0] + " and " + phrases[1]
